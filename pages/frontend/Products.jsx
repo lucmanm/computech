f5202069a7b59c8aaf1e8d8c7b3962e2d8c2aa23
lucmanm <greenjamez22@gmail.com>
2022-11-22 22:57:40 +0300

    Fetch API updated for products

--- pages/frontend/Products.jsx
+++ pages/frontend/Products.jsx
@@ -1,17 +1,84 @@
-import React from "react";
-
-import { ProductComp } from "./ProductComp";
+import React, { useEffect, useState } from "react";
+import aioimg from "./assets/img/aioimg.jpg";
+import { icons } from "./assets/data/data";
 export const Products = () => {
+  const [prod, setProd] = useState([]);
+
+  const apiUrl = "https://dummyjson.com/products";
+  // const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+  // let displayData;
+  // function apiFitch() {
+  //   fetch(apiUrl)
+  //     .then((response) => response.json())
+  //     .then((responseData) => {
+  //       displayData = responseData.map((prod) => {
+  //         return <p key={prod.id}>{prod.title}</p>;
+  //       });
+  //       console.log(displayData);
+  //       setProd(displayData);
+  //     });
+  //   // return;
+  // }
+  // const apiFitch = async () => {
+  //   const response = await fetch(apiUrl);
+  //   const responseData = await response.json();
+  //   displayData = responseData.products.map((prod) => {
+  //     return <p key={prod.id}>{prod.title}</p>;
+  //   });
+  //   // console.log(displayData);
+  //   setProd(displayData);
+  // };
+  const apiFitch = async () => {
+    const response = await fetch(apiUrl);
+    const responseData = await response.json();
+    // displayData = responseData.products.map((prod) => {
+    //   return <p key={prod.id}>{prod.title}</p>;
+    // });
+
+    setProd(responseData.products);
+  };
+  useEffect(() => {
+    apiFitch();
+  }, []);
+
   return (
     <div className="grid h-fit grid-cols-2  justify-evenly gap-3 tablet:grid-cols-3 desktop:container desktop:grid-cols-4">
-      <ProductComp />
-      <ProductComp />
-      <ProductComp />
-      <ProductComp />
-      <ProductComp />
-      <ProductComp />
-      <ProductComp />
-      <ProductComp />
+      {prod.slice(0, 8).map((prodDetails) => (
+        <div className="" key={prodDetails.id}>
+          <div className="group prose relative min-h-fit overflow-hidden rounded-md  bg-white p-7 hover:shadow-md">
+            <span className="absolute top-7 left-7 rounded-md border px-1 ring-1 ring-blue-800">
+              New
+            </span>
+            <img
+              className=" cursor-pointer"
+              src={aioimg}
+              alt="All in one Image"
+            />
+            {/* Wishlist, Compare, QuickView */}
+            <div className=" absolute top-7 right-7 flex flex-col items-end space-y-1">
+              <button className="invisible relative flex h-11  w-11 flex-row-reverse items-center overflow-hidden truncate rounded-md bg-blue-900 py-1 text-white opacity-0  duration-300 hover:w-32 hover:duration-500 group-hover:visible group-hover:opacity-100 group-hover:duration-300">
+                <span className="pr-2">{icons.heartIcon}</span>
+                <span className="mx-1 inline-block px-1 text-base font-semibold">
+                  Wish List
+                </span>
+              </button>
+            </div>
+            <span className="rounded-full bg-gray-100 px-2 py-1 text-blue-700">
+              Category
+            </span>
+            <h4 className="leading-6 line-clamp-2">
+              {prodDetails.description}
+            </h4>
+            <span className="block text-lg font-bold">
+              Sr {prodDetails.price}
+            </span>
+            <button className="container flex max-w-full justify-between rounded-md bg-blue-900 p-2 text-white hover:bg-blue-700">
+              <span>{icons.cartIcon}</span>
+              <span className=" pr-3">Add to cart</span>
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-gray-50">
-      <Header />
+      {/* <Header /> */}
       <IconNavigation />
       <Products />
       <Footer />
