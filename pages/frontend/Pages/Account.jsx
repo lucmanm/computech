04940fdfc670ce24c9953481f6aac022deabda98
lucmanm <greenjamez22@gmail.com>
2022-12-14 20:43:47 +0300

    updates

---
+++ pages/frontend/Pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Account = () => {
+  return (
+    <>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    </>
+  )
+}
