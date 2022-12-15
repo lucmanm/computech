6c23c32915e34a79e495492dda4b099a0508468a
lucmanm <greenjamez22@gmail.com>
2022-12-15 17:57:14 +0300

    Regular updates

--- pages/frontend/Header/Header.jsx
+++ pages/frontend/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Navigationbar } from "./Navigationbar";
 import { FrontSideNav } from "./FrontSideNav";
-import { LogoAndSearch } from "./LogoAndSearch";
+import { Link } from "react-router-dom";
 import { AccountLogin } from "./AccountLogin";
+import CompuTechLogo from "../assets/img/CompuTechLogo.png";
 export const Header = () => {
   return (
     <>
@@ -10,7 +11,26 @@ export const Header = () => {
         <div className=" bg-gray-800 py-2 text-center text-slate-50">
           Free Shipping all Over Saudi Arabia
         </div>
-        <LogoAndSearch />
+            {/* Computech Logo and Search fitler */}
+          <div className="relative flex items-center justify-evenly py-5 desktop:container">
+            <div className="shrink">
+              <Link to="/"><img src={CompuTechLogo} alt="" width={200} /></Link>
+            </div>
+              <div className="grow px-8">
+                <input
+                  className="min-w-full rounded-full border border-blue-900 text-base ring-1 ring-blue-900"
+                  type="text"
+                  name="Search"
+                  id=""
+                />
+              </div>
+            <div className="Help flex flex-col px-2 text-base font-medium">
+              <span className="text-blue-900">Need help? Call Us</span>
+              <span>+966 56 261 7554</span>
+            </div>
+          </div>
+          <div className="border border-b text-black shadow"></div>
+
         <div className="z-50 flex  w-screen  flex-row  items-center  py-1 desktop:container">
           <FrontSideNav />
           <Navigationbar />
--- pages/frontend/Pages/Account.jsx
+++ pages/frontend/Pages/Account.jsx
@@ -3,19 +3,18 @@ import { feAccount } from "../assets/data/data";
 import { NavLink } from "react-router-dom";
 export const Account = () => {
 
-
 const activeLink =
-  "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  "flex items-center gap-5 pl-4 pt-2 pb-2 rounded-lg text-white text-md m-2";
 const normalLink =
-  "flex items-center gap-5 pl-4 pt-4 pb-4 rounded-lg text-white text-md text-gray-700  hover:bg-blue-900 hover:text-white m-2";
+  "flex items-center gap-5 pl-4 pt-2 pb-2 rounded-lg text-white text-md text-gray-700  hover:bg-blue-900 hover:text-white m-2";
 
   return (
     <>
       <div className=" h-fit desktop:container grid grid-cols-9">
-        <div className="col-span-2">
+        <div className="col-span-2 my-5">
         {feAccount.map((accountTittle) => (
               <div key={accountTittle.title}>
-                <p className="m-3 mt-4 uppercase">
+                <p className="m-3 mt-2 uppercase font-semibold">
                   {accountTittle.title}
                 </p>
                 {/* Sub Categories Loops here. */}
