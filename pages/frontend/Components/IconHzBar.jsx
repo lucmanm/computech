3d3b1fa72eb2ba5b1a59d82b604a648fe262ec7d
lucmanm <greenjamez22@gmail.com>
2022-12-23 19:32:18 +0300

    Regular Updates

--- pages/frontend/Components/IconHzBar.jsx
+++ pages/frontend/Components/IconHzBar.jsx
@@ -6,16 +6,16 @@ export const IconHzBar = () => {
   return (
     <>
       <div className="fixed bottom-0 z-100 flex w-full flex-row items-center justify-evenly bg-blue-900 text-white shadow-xl shadow-gray-700 desktop:hidden">
-        <NavLink className="btn-icon btn-primary grow py-3" to="#">
+        <NavLink className="btn-icon btn-primary grow py-3" to="/">
           <div className="mx-auto w-fit">{homeIc}</div>
         </NavLink>
         <NavLink className="btn-icon btn-primary grow py-3" to="#">
           <div className="mx-auto w-fit">{searchIc}</div>
         </NavLink>
-        <NavLink className="btn-icon btn-primary grow py-3" to="#">
+        <NavLink className="btn-icon btn-primary grow py-3" to="/cart">
           <div className="mx-auto w-fit">{shopIc}</div>
         </NavLink>
-        <NavLink className="btn-icon btn-primary grow py-3" to="#">
+        <NavLink className="btn-icon btn-primary grow py-3" to="/login">
           <div className="mx-auto w-fit">{personIc}</div>
         </NavLink>
         <NavLink className="btn-icon btn-primary grow py-3" to="#">
