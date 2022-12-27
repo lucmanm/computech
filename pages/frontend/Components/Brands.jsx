bcba48a4f9875b5adbb57cebf894359586ef172e
lucmanm <greenjamez22@gmail.com>
2022-12-28 01:19:36 +0300

    regular Updates

--- pages/frontend/Components/Brands.jsx
+++ pages/frontend/Components/Brands.jsx
@@ -3,10 +3,10 @@ export const Brands = () => {
   return (
     <>
       <div className="m-2 desktop:container">
-        <div className="dr my-2 inline-block rounded-xl bg-blue-900 py-1 px-3 text-white drop-shadow">
+        <div className="my-2 inline-block rounded-xl bg-blue-900 py-1 px-3 text-white drop-shadow">
           Shop by Brand
         </div>
-        <div className="h-18 grid snap-x snap-mandatory grid-cols-6 gap-2">
+        <div className="h-18 grid snap-x snap-mandatory grid-cols-3 gap-2 desktop:grid-cols-6">
           <div className="h-18  cursor-pointer snap-start snap-always overflow-hidden rounded-md bg-blue-900">
             <img src={Banner596x249} alt="" />
           </div>
