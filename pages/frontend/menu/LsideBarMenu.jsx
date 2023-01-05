3f3c708cc5d7285dc223a9d423e60b2c8336a8a4
lucmanm <greenjamez22@gmail.com>
2023-01-05 12:56:54 +0300

    Regular Updates

--- pages/frontend/menu/LsideBarMenu.jsx
+++ pages/frontend/menu/LsideBarMenu.jsx
@@ -9,22 +9,24 @@ export const LsideBarMenu = () => {
   return (
     <>
       {/* Left sidebar Menu Option */}
-      {leftSideBar && (
-        <div className="fixed top-0 left-0 z-900 flex h-screen w-2/3 flex-col flex-wrap bg-gray-200 pb-14 desktop:hidden">
-          <button
-            className="absolute right-0 z-900 mt-6 mr-5 box-border cursor-pointer overflow-auto text-blue-900 hover:text-blue-700 focus:text-red-700 desktop:hidden"
-            onClick={(prevValue) => setLeftSideBar(!prevValue)}
-          >
-            {menuIc}
-          </button>
-          <div className="flex-1 border-black">
-            <div className="h-fit w-fit p-4">
-              <img src={CompuTechLogo} alt="Company Logo" className="w-1/2" />
-            </div>
-            <MobileMainMenu />
+      <div
+        className={`fixed top-0 left-0 z-900 flex h-screen duration-100 ${
+          leftSideBar ? "w-2/3" : "w-0"
+        } flex-col flex-wrap overflow-hidden bg-gray-200 pb-14 desktop:hidden`}
+      >
+        <button
+          className="absolute right-0 z-900 mt-6 mr-5 box-border cursor-pointer overflow-auto text-blue-900 hover:text-blue-700 focus:text-red-700 desktop:hidden"
+          onClick={(prevValue) => setLeftSideBar(!prevValue)}
+        >
+          {menuIc}
+        </button>
+        <div className="flex-1 border-black">
+          <div className="h-fit w-fit p-4">
+            <img src={CompuTechLogo} alt="Company Logo" className="w-1/2" />
           </div>
+          <MobileMainMenu />
         </div>
-      )}
+      </div>
     </>
   );
 };
--- pages/frontend/menu/SideBarMenu.jsx
+++ pages/frontend/menu/SideBarMenu.jsx
@@ -9,58 +9,54 @@ export const SideBarMenu = () => {
 
   return (
     <>
-      {sideMenuR && (
-        <div className="fixed top-0 z-900 h-full w-full  bg-slate-900 opacity-50">
-          wew
-        </div>
-      )}
-
-      {sideMenuR && (
-        <div className="fixed top-0 right-0 z-900 h-full w-3/4  bg-gray-100 pb-14">
-          <div className="bg-blue-900 px-2 py-3 font-bold text-white">MENU</div>
+      <div
+        className={`fixed top-0 right-0 z-900 h-full ${
+          sideMenuR ? "w-2/3" : "w-0"
+        } overflow-hidden  bg-gray-100 pb-14 duration-100`}
+      >
+        <div className="bg-blue-900 px-2 py-3 font-bold text-white">MENU</div>
+        {loginAuth && (
+          // #LoggedInOnly
+          <div>
+            <AccountMenu />
+          </div>
+        )}
+        <div className="flex flex-col">
+          {/* Sub Categories Loops here. */}
+          <div className="md-btn-nav-container">
+            {menu.map(({ name, path }) => (
+              <NavLink
+                to={`${path}`}
+                key={path}
+                className="md-btn-nav"
+                onClick={() =>
+                  sideMenuR
+                    ? setSideMenuR((prevActiveMenu) => !prevActiveMenu)
+                    : ""
+                }
+              >
+                <span className="capatilize">{name}</span>
+              </NavLink>
+            ))}
+          </div>
+          {/* Product Category Menu */}
+          <MobileMainMenu />
           {loginAuth && (
-            // #LoggedInOnly
             <div>
-              <AccountMenu />
+              <Link
+                to="/"
+                className="flex cursor-pointer items-center space-x-6 bg-blue-900 py-3 px-2 font-bold text-white hover:bg-red-700 hover:text-white"
+                onClick={() => {
+                  localStorage.clear();
+                  setSideMenuR((prevActiveMenu) => !prevActiveMenu);
+                }}
+              >
+                <p>Logout</p>
+              </Link>
             </div>
           )}
-          <div className="flex flex-col">
-            {/* Sub Categories Loops here. */}
-            <div className="md-btn-nav-container">
-              {menu.map(({ name, path }) => (
-                <NavLink
-                  to={`${path}`}
-                  key={path}
-                  className="md-btn-nav"
-                  onClick={() =>
-                    sideMenuR
-                      ? setSideMenuR((prevActiveMenu) => !prevActiveMenu)
-                      : ""
-                  }
-                >
-                  <span className="capatilize">{name}</span>
-                </NavLink>
-              ))}
-            </div>
-            {/* Product Category Menu */}
-            <MobileMainMenu />
-            {loginAuth && (
-              <div>
-                <Link
-                  to="/"
-                  className="flex cursor-pointer items-center space-x-6 bg-blue-900 py-3 px-2 font-bold text-white hover:bg-red-700 hover:text-white"
-                  onClick={() => {
-                    localStorage.clear();
-                    setSideMenuR((prevActiveMenu) => !prevActiveMenu);
-                  }}
-                >
-                  <p>Logout</p>
-                </Link>
-              </div>
-            )}
-          </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
